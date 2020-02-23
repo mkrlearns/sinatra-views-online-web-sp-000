c34f2b282f93668652482164ac9b3377a0917809
ryan m <contact@ryanmeek.com>
2020-02-23 17:40:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,3 +1,3 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base; get('/') { erb :index }; get('/info') { erb:info } end
+class App < Sinatra::Base; get('/') {erb :index}; get('/info') {erb:info} end
